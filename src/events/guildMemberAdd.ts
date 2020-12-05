d5176728d1ba39fffa3d48870fc5a411793ef308
Amit Bar <amitbar00@gmail.com>
2020-12-05 02:15:51 -0800

    deleted unused file

--- src/events/guildMemberAdd.ts
+++
@@ -1,6 +0,0 @@
-import { Client, Message } from 'discord.js';
-import client from '../index';
-
-client.on("guildMemberAdd", (member) => {
-  member.send(`Waddup bro. Who do you know here?!?`);
-});
